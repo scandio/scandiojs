9b2ebbd77808b46ae119392ca6c4ecb7b153805a
Tobias Deekens <tobias.deekens@gmail.com>
2013-08-20 17:37:44 +0200

    Adds specs for ß.util.breaks-fn.

--- test/specs/util.js
+++ test/specs/util.js
@@ -201,4 +201,55 @@ describe("A suite testing the utility functions", function() {
          });
       });
    });
+
+   describe("tests on putting the breaks on a function call for n-milliseconds", function() {
+      it("should break the execution of a function", function() {
+         runs(function() {
+            this.counter = 0;
+
+            this.breakedIncrementor = ß.util.breaks(function() {
+               this.counter++;
+            }, 42);
+
+            this.breakedIncrementor();
+            this.breakedIncrementor();
+
+            //Not called
+            expect(this.counter).toEqual(0);
+         });
+
+         waits(50);
+
+         runs(function() {
+            expect(this.counter).toEqual(1);
+         });
+      });
+
+      it("should break the execution of a function with arguments", function() {
+         runs(function() {
+            this.counter = 0;
+
+            this.breakedIncrementor = ß.util.breaks(function(to) {
+               this.counter = to;
+            }, 10);
+
+            this.breakedIncrementor(5);
+            this.breakedIncrementor(10);
+
+            //Not called
+            expect(this.counter).toEqual(0);
+
+            ß.util.delay(function() {
+               //Called once now
+               expect(that.counter).toEqual(5);
+            }, 15);
+         });
+
+         waits(15);
+
+         runs(function() {
+            expect(this.counter).toEqual(10);
+         });
+      });
+   });
 });
