e9a83f88a0204d523d890de87cac1d1e974c4a96
Tobias Deekens <tobias.deekens@gmail.com>
2013-08-27 12:39:41 +0200

    Moves ß.messenger spec into ß.core as it is part of it.

--- test/specs/core.js
+++ test/specs/core.js
@@ -1,7 +1,48 @@
 describe("A suite testing the core module", function() {
+   describe("tests on messenger (pub/sub) functionality", function() {
+      var callbacks = {};
 
-   it("should have an example test", function() {
-      expect(false).not.toBe(true);
+      beforeEach(function() {
+         callbacks = {
+            handler1: function(event, arg1, arg2) {
+               console.log("foo.bar", arg1, arg2);
+            },
+            handler2: function(event, arg1) {
+               console.log("foo", arg1);
+            }
+         };
+
+         spyOn(callbacks, 'handler1');
+         spyOn(callbacks, 'handler2');
+      });
+
+      it("should allow for fully qualified subscriptions", function() {
+         ß.messenger.subscribe('foo.bar', callbacks.handler1);
+         ß.messenger.publish('foo.bar');
+
+         expect(callbacks.handler1).toHaveBeenCalled();
+         expect(callbacks.handler2).not.toHaveBeenCalled();
+      });
+
+      it("should allow for fuzzy (unqualified) subscriptions", function() {
+         ß.messenger.subscribe('foo', callbacks.handler2);
+         ß.messenger.subscribe('foo.bar', callbacks.handler1);
+
+         ß.messenger.publish('foo');
+
+         expect(callbacks.handler1).toHaveBeenCalled();
+         expect(callbacks.handler2).toHaveBeenCalled();
+      });
+
+      it("should allow for unsubscribing from messenger", function() {
+         ß.messenger.subscribe('foo.bar', callbacks.handler1);
+         ß.messenger.subscribe('foo', callbacks.handler2);
+
+         ß.messenger.unsubscribe('foo.bar');
+         ß.messenger.publish('foo');
+
+         expect(callbacks.handler1).not.toHaveBeenCalled();
+         expect(callbacks.handler2).toHaveBeenCalled();
+      });
    });
-
 });
