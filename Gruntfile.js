08d541e430ea3c16ef14956bb15c6ee9218ebb81
Tobias Deekens <nerd@tdeekens.name>
2013-11-19 22:18:37 +0100

    Adds compatibility.js to Gruntfile.js.

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,6 +21,7 @@ module.exports = function( grunt ) {
          },
          src: [
             "js/index.js",
+            "js/compatibility.js",
             "js/dom.js",
             "js/string.js",
             "js/logger.js",
