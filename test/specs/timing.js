a26c6bad98bc1835fa7f8cc379a5db05803737d9
Tobias Deekens <nerd@tdeekens.name>
2014-03-09 23:32:21 +0100

    Updates timing & ajax specs to Jasmine 2.0.

--- test/specs/timing.js
+++ test/specs/timing.js
@@ -1,75 +1,67 @@
 describe("A suite testing the timing functions", function() {
    describe("tests on delaying a function call for n-milliseconds", function() {
+      beforeEach(function(done) {
+         var that = this;
+         this.hasBeenCalled = false;
+
+         Scandio.timing.delay(function() {
+            that.hasBeenCalled = true;
+
+            done();
+         }, 50);
+      });
+
       it("should defer the execution of a function", function() {
-         runs(function() {
-            this.hasBeenCalled = false;
-            var that = this;
-
-            Scandio.timing.delay(function() {
-               that.hasBeenCalled = true;
-            }, 50);
-         });
-
-         runs(function() {
-            expect(this.hasBeenCalled).toBe(false);
-         });
-
-         waits(100);
-
-         runs(function() {
-            expect(this.hasBeenCalled).toBe(true);
-         });
+         expect(this.hasBeenCalled).toBe(true);
       });
    });
 
-   describe("tests on putting the breaks on a function call for n-milliseconds", function() {
-      it("should break the execution of a function", function() {
-         runs(function() {
-            this.counter = 0;
+   describe("tests on putting the breaks on a function call for n-milliseconds without args", function() {
+      beforeEach(function(done) {
+         this.counter = 0;
 
-            this.breakedIncrementor = Scandio.timing.breaks(function() {
-               this.counter++;
-            }, 42);
+         this.breakedIncrementor = Scandio.timing.breaks(function() {
+            this.counter++;
 
-            this.breakedIncrementor();
-            this.breakedIncrementor();
+            done();
+         }, 42);
 
-            //Not called
-            expect(this.counter).toEqual(0);
-         });
+         this.breakedIncrementor();
+         this.breakedIncrementor();
 
-         waits(50);
-
-         runs(function() {
-            expect(this.counter).toEqual(1);
-         });
+         //Not called
+         expect(this.counter).toEqual(0);
       });
 
-      it("should break the execution of a function with arguments", function() {
-         runs(function() {
-            this.counter = 0;
-            var that = this;
+      it("should defer the execution of a function", function() {
+         expect(this.counter).toEqual(1);
+      });
+   });
 
-            this.breakedIncrementor = Scandio.timing.breaks(function(to) {
-               this.counter = to;
-            }, 10);
+   describe("tests on putting the breaks on a function call for n-milliseconds with args", function() {
+      beforeEach(function(done) {
+         this.counter = 0;
+         var that = this;
 
-            this.breakedIncrementor(10);
+         this.breakedIncrementor = Scandio.timing.breaks(function(to) {
+            this.counter = to;
 
-            //Not called
-            expect(this.counter).toEqual(0);
+            done();
+         }, 10);
 
-            Scandio.timing.delay(function() {
-               //Called once now
-               expect(that.counter).toEqual(10);
-            }, 10);
-         });
+         this.breakedIncrementor(10);
 
-         waits(10);
+         //Not called
+         expect(this.counter).toEqual(0);
 
-         runs(function() {
-            expect(this.counter).toEqual(10);
-         });
+         Scandio.timing.delay(function() {
+            //Called once now
+            expect(that.counter).toEqual(10);
+         }, 10);
+      });
+
+      it("should defer the execution of a function", function() {
+         expect(this.counter).toEqual(10);
       });
    });
 });
